c415f7ad88c7bfb18a1b61e905109bbb6ea23b81
Jean Lima <93737943+JeanLimaa@users.noreply.github.com>
2023-11-05 09:47:12 -0300

    Questão 16

---
+++ programas/Equipe8-Questao16-2023-11-13.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    double numerador, denominador, resultado_da_fracao, acumulador;
+
+    for(int i = 1; i < 9; i++){
+        numerador = pow(2, 3*i+pow(i, 2));
+        denominador = i + 1;
+
+        resultado_da_fracao = numerador / denominador;
+
+        acumulador +=  resultado_da_fracao;
+    }
+
+    prtinf("%2.f", acumulador);
+}
